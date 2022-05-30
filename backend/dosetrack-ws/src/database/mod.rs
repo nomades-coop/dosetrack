4a133f93ff4522b25875e200962062a1dfbd8ed3
Pablo Centurión <pablushka@nomades.com.ar>
2022-05-30 20:43:28 -0300

    Comment  all unused sample code

--- backend/dosetrack-ws/src/database/mod.rs
+++ backend/dosetrack-ws/src/database/mod.rs
@@ -20,64 +20,64 @@ impl MongoDB {
         self.database.collection::<T>(col)
     }
 
-    pub async fn fetch_all_published_blogs(
-        &self,
-        filter_drafts: bool,
-    ) -> mongodb::error::Result<Vec<BlogEntry>> {
-        let collection = self.database.collection::<BlogEntry>("blogs");
-        // Query the blogs in the collection which are published.
-        let filter = match filter_drafts {
-            true => Some(doc! { "status": "Published" }),
-            false => None,
-        };
-        let find_options = FindOptions::builder().sort(doc! { "title": 1 }).build(); //TODO sort by date
+    // pub async fn fetch_all_published_blogs(
+    //     &self,
+    //     filter_drafts: bool,
+    // ) -> mongodb::error::Result<Vec<BlogEntry>> {
+    //     let collection = self.database.collection::<BlogEntry>("blogs");
+    //     // Query the blogs in the collection which are published.
+    //     let filter = match filter_drafts {
+    //         true => Some(doc! { "status": "Published" }),
+    //         false => None,
+    //     };
+    //     let find_options = FindOptions::builder().sort(doc! { "title": 1 }).build(); //TODO sort by date
 
-        let mut cursor: Cursor<BlogEntry> = collection.find(filter, find_options).await?;
+    //     let mut cursor: Cursor<BlogEntry> = collection.find(filter, find_options).await?;
 
-        let mut blogs: Vec<BlogEntry> = Vec::new();
-        while let Some(blog) = cursor.try_next().await? {
-            blogs.push(blog);
-        }
+    //     let mut blogs: Vec<BlogEntry> = Vec::new();
+    //     while let Some(blog) = cursor.try_next().await? {
+    //         blogs.push(blog);
+    //     }
 
-        Ok(blogs)
-    }
+    //     Ok(blogs)
+    // }
 
-    pub async fn find_blog(&self, id: String) -> mongodb::error::Result<BlogEntry> {
-        let collection = self.database.collection::<BlogEntry>("blogs");
-        Ok(collection
-            .find_one(doc! { "_id": id }, None)
-            .await?
-            .unwrap())
-    }
+    // pub async fn find_blog(&self, id: String) -> mongodb::error::Result<BlogEntry> {
+    //     let collection = self.database.collection::<BlogEntry>("blogs");
+    //     Ok(collection
+    //         .find_one(doc! { "_id": id }, None)
+    //         .await?
+    //         .unwrap())
+    // }
 
-    pub async fn delete_blog(&self, id: String) -> mongodb::error::Result<()> {
-        let collection = self.database.collection::<BlogEntry>("blogs");
-        collection.delete_one(doc! { "_id": id }, None).await?;
-        Ok(())
-    }
+    // pub async fn delete_blog(&self, id: String) -> mongodb::error::Result<()> {
+    //     let collection = self.database.collection::<BlogEntry>("blogs");
+    //     collection.delete_one(doc! { "_id": id }, None).await?;
+    //     Ok(())
+    // }
 
-    pub async fn update_blog(&self, blog: &mut BlogEntry) -> mongodb::error::Result<String> {
-        let collection = self.database.collection::<BlogEntry>("blogs");
-        let _result = collection
-            .replace_one(doc! { "_id":  &blog._id }, blog, None)
-            .await?;
-        Ok("ok".to_string())
-    }
+    // pub async fn update_blog(&self, blog: &mut BlogEntry) -> mongodb::error::Result<String> {
+    //     let collection = self.database.collection::<BlogEntry>("blogs");
+    //     let _result = collection
+    //         .replace_one(doc! { "_id":  &blog._id }, blog, None)
+    //         .await?;
+    //     Ok("ok".to_string())
+    // }
 
-    pub async fn add_blog(&self, blog: &mut BlogEntry) -> mongodb::error::Result<String> {
-        let collection = self.database.collection::<BlogEntry>("blogs");
-        blog._id = bson::oid::ObjectId::new().to_string();
-        let insert: InsertOneResult = collection.insert_one(blog, None).await?;
-        Ok(insert.inserted_id.to_string())
-    }
+    // pub async fn add_blog(&self, blog: &mut BlogEntry) -> mongodb::error::Result<String> {
+    //     let collection = self.database.collection::<BlogEntry>("blogs");
+    //     blog._id = bson::oid::ObjectId::new().to_string();
+    //     let insert: InsertOneResult = collection.insert_one(blog, None).await?;
+    //     Ok(insert.inserted_id.to_string())
+    // }
 
-    pub async fn is_authorized(&self, user_id: &String) -> mongodb::error::Result<bool> {
-        let authorization = self.database.collection::<Document>("authorization");
-        Ok(authorization
-            .count_documents(doc! { "_id": user_id }, None)
-            .await?
-            == 1)
-    }
+    // pub async fn is_authorized(&self, user_id: &String) -> mongodb::error::Result<bool> {
+    //     let authorization = self.database.collection::<Document>("authorization");
+    //     Ok(authorization
+    //         .count_documents(doc! { "_id": user_id }, None)
+    //         .await?
+    //         == 1)
+    // }
 
     // pub async fn fetch_all_users(&self) -> mongodb::error::Result<Vec<User>> {
     //     let collection = self.database.collection::<User>("users");
