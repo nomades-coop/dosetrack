1dc0a63331f599b8044ef34b2a7a8b1bf9cd2b8c
Pablo Centurión <pablushka@nomades.com.ar>
2023-03-20 01:55:38 -0300

    Fix cache adding reload option

--- frontend/dosetrack/src/services/cache.js
+++ frontend/dosetrack/src/services/cache.js
@@ -1,25 +1,25 @@
 
-export const getCachedData = async (key, fetchEndpoint) => {
-  let cache_data = localStorage.getItem(key); 
-  
-  if (cache_data) {
+export const getCachedData = async (key, fetchEndpoint, reload = false) => {
+  let cache_data = localStorage.getItem(key);
+
+  if (cache_data && !reload) {
 
     try {
       cache_data = JSON.parse(cache_data);
-      if (Math.floor((Date.now() - cache_data['timestamp']) /1000) < 30) {
+      if (Math.floor((Date.now() - cache_data['timestamp']) / 1000) < 30) {
         return cache_data['data']
       }
     } catch (e) {
       return null;
     }
 
-  } 
-  
+  }
+
   const res = await fetch(fetchEndpoint);
-      
+
   if (res.ok) {
     let list = await res.json();
-    let data = {'timestamp': Date.now(), 'data': list}
+    let data = { 'timestamp': Date.now(), 'data': list }
     localStorage.setItem(key, JSON.stringify(data));
     return list;
   } else {
